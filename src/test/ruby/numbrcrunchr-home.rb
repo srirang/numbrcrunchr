d2f5b5ffea2934648e704345b3cf9674786d5768
Srinivasan Ranganathan <srinirvasan@gmail.com>
2014-03-05 00:51:29 +1100

    Started adding some functional tests

---
+++ src/test/ruby/numbrcrunchr-home.rb
@@ -0,0 +1,14 @@
+require 'watir-webdriver'
+b = Watir::Browser.new :ie
+b.goto 'http://localhost:8081/numbrcrunchr'
+
+b.text.include? 'NumbrCrunchr - Investment Property Number Cruncher'
+b.text.include? 'Step 1 : Property Details'
+b.text.include? 'Step 2 : Income Details'
+b.text.include? 'Investment Summary'
+b.text.include? '+ Change Assumptions'
+
+b.select_list(:id => 'feasibilityAnalysisForm:State').select 'NSW'
+b.text_field(:id => 'feasibilityAnalysisForm:Price').set '320000'
+b.text_field(:id => 'feasibilityAnalysisForm:WeeklyRent').set '320'
+b.button(:id => 'feasibilityAnalysisForm:CrunchNumbers').click
